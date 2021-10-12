b267bd75f8830d7139b518413725ea2d2e6fa4f0
dimakravets603 <89866264+dimakravets603@users.noreply.github.com>
2021-10-12 18:20:09 +0300

    TODO List

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-(
-    function() {
-        document.body.innerHTML+= "Hello world"
+function newElement() {
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("myInput").value;
+    let aggy = document.createTextNode(inputValue);
+   li.appendChild(aggy);
+    {
+      document.getElementById("myUL").appendChild(li);
     }
-)();
+    document.getElementById("myInput").value = "";}
