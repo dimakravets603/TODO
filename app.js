fef62dc18424e96b5d8a0f8fb1d5c78a2d779a45
dimakravets603 <89866264+dimakravets603@users.noreply.github.com>
2021-11-15 22:46:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function newElement() {
     const li = document.createElement("li");
     const inputValue = document.getElementById("myInput").value;
     const text = document.createTextNode(inputValue);
-   li.appendChild(aggy);
+   li.appendChild(text);
     {
       document.getElementById("myUL").appendChild(li);
     }
